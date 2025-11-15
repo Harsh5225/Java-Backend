17b836ab9cf237ffba7d6450c92a9b7fb252052f
Harsh <harshsingh94070@gmail.com>
2025-11-15 22:35:01 +0530

    Step 2: Added User and Role model entities

---
+++ springSecurityProj/src/main/java/com/spring_rbac/springSecurityProj/Model/User.java
@@ -0,0 +1,69 @@
+package com.spring_rbac.springSecurityProj.Model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private  String email;
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id")
+    )
+    private Set<Role>roles;
+
+    public User() {
+    }
+
+    public User(String name, String email, Set<Role> roles) {
+        this.name = name;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
